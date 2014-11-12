9fc7e2b64b5c01c356ba06c910eb7a11a97a0521
verena91 <vereojeda@gmail.com>
2014-11-12 16:53:20 -0300

    Cuartiles

---
+++ src/main/resources/db/cuartiles.sql
@@ -0,0 +1,39 @@
+-- DISTRITOS
+create table cuartiles_por_distrito (
+	id int,
+	departamento character varying,
+	distrito character varying,
+	cuartil_uno character varying,
+	cuartil_dos character varying,
+	cuartil_tres character varying,
+	tipo character varying
+)
+
+COPY cuartiles_por_distrito (id, departamento, distrito, cuartil_uno, cuartil_dos, cuartil_tres, tipo) 
+FROM '/home/verena/dengue-maps-server/src/main/resources/db/cuartiles_por_distrito.csv' 
+DELIMITER ';' CSV HEADER;
+
+ALTER TABLE public.cuartiles_por_distrito OWNER TO postgres;
+
+ALTER TABLE ONLY cuartiles_por_distrito
+    ADD CONSTRAINT cuartiles_por_distrito_pkey PRIMARY KEY (id);
+
+
+-- BARRIOS
+create table cuartiles_por_barrio (
+	id int,
+	barrio character varying,
+	area character varying,
+	cuartil_uno character varying,
+	cuartil_dos character varying,
+	cuartil_tres character varying
+)
+
+COPY cuartiles_por_barrio (id, barrio, area, cuartil_uno, cuartil_dos, cuartil_tres) 
+FROM '/home/verena/dengue-maps-server/src/main/resources/db/cuartiles_por_barrio.csv' 
+DELIMITER ';' CSV HEADER;
+
+ALTER TABLE public.cuartiles_por_barrio OWNER TO postgres;
+
+ALTER TABLE ONLY cuartiles_por_barrio
+    ADD CONSTRAINT cuartiles_por_barrio_pkey PRIMARY KEY (id);
