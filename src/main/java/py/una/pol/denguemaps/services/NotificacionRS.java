9abd8324ba00e84194dc6c2a243acabab76ca5ab
Verena Ojeda <vereojeda@gmail.com>
2014-11-20 19:08:24 -0300

    Clean del pyto.

--- src/main/java/py/una/pol/denguemaps/services/NotificacionRS.java
+++ src/main/java/py/una/pol/denguemaps/services/NotificacionRS.java
@@ -39,18 +39,18 @@ public class NotificacionRS implements NotificacionAPI {
 	/** Métodos de la API **/
 
 	@Override
-	public Response getNotificaciones(int page, String anio, String semana, String fechaNotificacion,
-			String departamento, String distrito, String sexo, String edad,
-			String resultado, int limit, String sortField,
-			String sortOrder, Notificacion notificacion) {
-		System.out.println("+++++++++++++++++ Entro al servicio de notificaciones paginadas");
+	public Response getNotificaciones(int page, String anio, String semana,
+			String fechaNotificacion, String departamento, String distrito,
+			String sexo, String edad, String resultado, int limit,
+			String sortField, String sortOrder, Notificacion notificacion) {
+		System.out
+				.println("+++++++++++++++++ Servicio de notificaciones paginadas");
 		List<Notificacion> finalList = new ArrayList<Notificacion>();
 		PagedList<Notificacion> lista = new PagedList<Notificacion>();
-		//PagedList<String[]> lista = new PagedList<String[]>();
-		
-		System.out.println("+++++++++++++++++ sortField " + sortField);
-		System.out.println("+++++++++++++++++ sortOrder " + sortOrder);
-		
+
+		// System.out.println("+++++++++++++++++ sortField " + sortField);
+		// System.out.println("+++++++++++++++++ sortOrder " + sortOrder);
+
 		if (sortField.compareTo("0") == 0) {
 			sortField = "anio";
 		} else if (sortField.compareTo("1") == 0) {
@@ -70,7 +70,7 @@ public class NotificacionRS implements NotificacionAPI {
 		} else {
 			sortField = "id";
 		}
-		
+
 		System.out.println("+++" + anio + "+++");
 		System.out.println("+++" + semana + "+++");
 		System.out.println("+++" + departamento + "+++");
@@ -78,47 +78,46 @@ public class NotificacionRS implements NotificacionAPI {
 		System.out.println("+++" + resultado + "+++");
 		System.out.println("+++" + sexo + "+++");
 		System.out.println("+++" + edad + "+++");
-		
+
 		Notificacion filtro = new Notificacion();
-		
+
 		boolean tieneFiltro = false;
-		
+
 		filtro.setId(null);
-		
-		if (anio != null && anio.compareTo("")!=0) {
+
+		if (anio != null && anio.compareTo("") != 0) {
 			filtro.setAnio(anio);
 			tieneFiltro = true;
 		}
-		if (semana != null && semana.compareTo("")!=0) {
+		if (semana != null && semana.compareTo("") != 0) {
 			filtro.setSemana(semana);
 			tieneFiltro = true;
 		}
-		if (fechaNotificacion != null && fechaNotificacion.compareTo("")!=0) {
+		if (fechaNotificacion != null && fechaNotificacion.compareTo("") != 0) {
 			filtro.setFecha_notificacion(fechaNotificacion);
 			tieneFiltro = true;
 		}
-		if (departamento != null && departamento.compareTo("")!=0) {
+		if (departamento != null && departamento.compareTo("") != 0) {
 			filtro.setDepartamento(departamento);
 			tieneFiltro = true;
 		}
-		if (distrito != null && distrito.compareTo("")!=0) {
+		if (distrito != null && distrito.compareTo("") != 0) {
 			filtro.setDistrito(distrito);
 			tieneFiltro = true;
 		}
-		if (sexo != null && sexo.compareTo("")!=0) {
+		if (sexo != null && sexo.compareTo("") != 0) {
 			filtro.setSexo(sexo);
 			tieneFiltro = true;
 		}
-		if (edad != null && edad.compareTo("")!=0) {
+		if (edad != null && edad.compareTo("") != 0) {
 			filtro.setEdad(edad);
 			tieneFiltro = true;
 		}
-		if (resultado!= null && resultado.compareTo("")!=0) {
+		if (resultado != null && resultado.compareTo("") != 0) {
 			filtro.setClasificacon_clinica(resultado);
 			tieneFiltro = true;
 		}
 
-		
 		Pagination pag = null;
 
 		// paginar o no
@@ -135,32 +134,15 @@ public class NotificacionRS implements NotificacionAPI {
 				asc = false;
 			}
 		}
-		
-		if (tieneFiltro){
-			finalList = notificacionBC.findAll(sortField, asc, /*notificacion*/filtro, pag);	
+
+		if (tieneFiltro) {
+			finalList = notificacionBC.findAll(sortField, asc, filtro, pag);
 		} else {
 			finalList = notificacionBC.findAll(sortField, asc, null, pag);
 		}
-		
-		
-		List<String[]> list = new ArrayList<String[]>();
-		for (Notificacion not : finalList) {
-			String[] row = new String[8];
-			//row[0] = not.getId() + "";
-			row[0] = not.getAnio();
-			row[1] = not.getSemana();
-			row[2] = not.getFecha_notificacion();
-			row[3] = not.getDepartamento();
-			row[4] = not.getDistrito();
-			row[5] = not.getSexo();
-			row[6] = not.getEdad();
-			row[7] = not.getClasificacon_clinica();
-			list.add(row);
-		}
 
 		lista.setData(finalList);
-		//lista.setData(list);
-		
+
 		if (pag != null) {
 			lista.setRecordsTotal(pag.getTotalResults());
 			lista.setDraw(page);
@@ -181,6 +163,8 @@ public class NotificacionRS implements NotificacionAPI {
 
 	@Override
 	public Response getJsonNotificacionesPorAnio(String anio) {
+		System.out
+				.println("+++++++++++++++++ Servicio de notificaciones por año");
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			list = n.executeNativeQueryFromFileParameters(queryConfig.getRoot()
@@ -192,8 +176,10 @@ public class NotificacionRS implements NotificacionAPI {
 		}
 		return Response.ok(list).build();
 	}
-	
-	public Response getJsonRiesgosPorAnio(String anio){
+
+	public Response getJsonRiesgosPorAnio(String anio) {
+		System.out
+				.println("+++++++++++++++++ Servicio de riesgos por departamento");
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			list = n.executeNativeQueryFromFileParameters(queryConfig.getRoot()
@@ -208,6 +194,8 @@ public class NotificacionRS implements NotificacionAPI {
 
 	@Override
 	public Response getJsonRiesgosDistritoPorAnio(String anio) {
+		System.out
+				.println("+++++++++++++++++ Servicio de riesgos por distrito");
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			list = n.executeNativeQueryFromFileParameters(queryConfig.getRoot()
@@ -222,6 +210,8 @@ public class NotificacionRS implements NotificacionAPI {
 
 	@Override
 	public Response getJsonRiesgosAsuncionPorAnio(String anio) {
+		System.out
+				.println("+++++++++++++++++ Servicio de riesgos por barrios de Asunción");
 		List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 		try {
 			list = n.executeNativeQueryFromFileParameters(queryConfig.getRoot()
@@ -235,18 +225,11 @@ public class NotificacionRS implements NotificacionAPI {
 	}
 
 	@Override
-	public Response getNotificacionesPorAnio(String anio) {
-		System.out.println("+++++++ servicios de notificaciones por anio");
-		List<Notificacion> list = notificacionBC.getNotificacionesPorAnio(anio);
-
-		return Response.ok(list).build();
-	}
-
-	@Override
 	public Response getNotificacionesFiltradas(String anio, String semana,
 			String fechaNotificacion, String departamento, String distrito,
 			String sexo, String edad, String resultado) {
-		System.out.println("+++++++ servicios de notificaciones filtradas");
+		System.out
+				.println("+++++++++++++++++ Servicio de notificaciones filtradas");
 		List<Notificacion> list = notificacionBC.getNotificacionesFiltradas(
 				anio, semana, fechaNotificacion, departamento, distrito, sexo,
 				edad, resultado);
@@ -257,17 +240,17 @@ public class NotificacionRS implements NotificacionAPI {
 	@Override
 	public Response getJsonNotificacionesPorFiltros(String anio, String sexo,
 			String resultado) {
-		String query = "select count(*), semana, anio, from notificacion where anio = '"+anio+"'";
-		if(sexo!=null){
-			query = query + "and sexo = "+sexo;
+		String query = "select count(*), semana, anio, from notificacion where anio = '"
+				+ anio + "'";
+		if (sexo != null) {
+			query = query + "and sexo = " + sexo;
 		}
-		if(resultado!=null){
-			
+		if (resultado != null) {
+
 		}
-			
-		query = query +  "group by semana";
+
+		query = query + "group by semana";
 		return null;
 	}
-	
 
 }
